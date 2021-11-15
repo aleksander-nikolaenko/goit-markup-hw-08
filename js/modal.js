e2060d0f775a5d278bfbdf008f20f3e2654af3aa
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2021-11-15 20:27:50 +0200

    убирает прокрутку сайта если открыто модальное окно

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,8 @@
     openModalBtn: document.querySelector('.hero__btn'),
     closeModalBtn: document.querySelector('.close-btn'),
     modal: document.querySelector('.backdrop'),
-    modalItem: document.querySelector('.modal')
+    modalItem: document.querySelector('.modal'),
+    scroll: document.querySelector('body')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -13,10 +14,12 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+     refs.scroll.classList.toggle('no-scroll');
   }
   function removeModal(e) {
     if (e.target === refs.modal) {
       refs.modal.classList.add('is-hidden');
+      refs.scroll.classList.toggle('no-scroll');
     }
   }
 })();
